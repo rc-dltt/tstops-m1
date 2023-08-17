24b18675b8cba242d86ef1168c882a54ac71b30b
rc-dltt <ricorsini@deloitte.com.hk>
2023-08-17 22:48:00 +0800

    add subs

---
+++ subscriptions/src/context.ts
@@ -0,0 +1,26 @@
+import { verify } from "jsonwebtoken";
+import { users } from "./db";
+
+const getPrincipal = (token) => {
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET);
+    return decoded.id;
+  } catch (err) {
+    return null;
+  }
+};
+
+export const buildContext = async ({ req }) => {
+  const token = (req.headers && req.headers["x-access-token"]) ?? "";
+
+  const principal = getPrincipal(token);
+
+  const dataSources = {
+    users,
+  };
+
+  return {
+    dataSources,
+    principal,
+  };
+};
---
+++ subscriptions/src/index.ts
@@ -0,0 +1,68 @@
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { createServer } from "http";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
+import { ApolloServer } from "@apollo/server";
+import { readFileSync } from "node:fs";
+import { config } from "dotenv";
+
+import { buildContext } from "./context";
+import { resolvers } from "./resolvers";
+
+config();
+
+const typeDefs = readFileSync("./src/schema.graphql", "utf8");
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const app = express();
+const httpServer = createServer(app);
+
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: "/graphql",
+});
+const serverCleanup = useServer({ schema }, wsServer);
+
+const server = new ApolloServer({
+  schema,
+  plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+  ],
+});
+
+async function start() {
+  await server.start();
+
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    expressMiddleware(server, {
+      context: buildContext
+    })
+  );
+
+  const PORT = process.env.PORT || "4003";
+
+  httpServer.listen(PORT || "4003", () => {
+    console.log(`🚀 Query endpoint ready at http://localhost:${PORT}/graphql`);
+    console.log(`🚀 Subscription endpoint ready at ws://localhost:${PORT}/graphql`);
+  });
+}
+
+start();
