377d6629edda7a69949fc9f765915ab7923ec281
rc-dltt <ricorsini@deloitte.com.hk>
2023-08-17 11:11:06 +0800

    add context and pass-thru headers

--- server/final/gateway/src/index.js
+++ server/final/gateway/src/index.js
@@ -1,15 +1,36 @@
 const { ApolloServer } = require("apollo-server");
-const { ApolloGateway } = require("@apollo/gateway");
+const { ApolloGateway, RemoteGraphQLDataSource } = require("@apollo/gateway");
 const { readFileSync } = require("fs");
+
+class AuthDataSource extends RemoteGraphQLDataSource {
+  async willSendRequest({ request, context }) {
+    const headers = context.req?.headers;
+    if (headers) {
+      for (const key in headers) {
+        const value = headers[key];
+        if (value) {
+          request.http?.headers?.set(key, String(value));
+        }
+      }
+    }
+  }
+}
 
 const supergraphSdl = readFileSync("./src/supergraph.graphql").toString();
 
 const gateway = new ApolloGateway({
   supergraphSdl,
+  buildService({ name, url }) {
+    return new AuthDataSource({ url });
+  },
 });
 
 const server = new ApolloServer({
   gateway,
+  context: ({ req, res }) => ({
+    req,
+    res
+  }),
 });
 
 const PORT = process.env.PORT || 4000;
